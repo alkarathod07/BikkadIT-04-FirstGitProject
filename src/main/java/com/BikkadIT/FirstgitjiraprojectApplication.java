b767390928dcf9c6baf5c0672472ee2bbeb5f9e0
alkarathod07 <alkarathod.rathod1@gmail.com>
2022-11-11 17:32:32 +0530

    FIR 1:create project in springboot

---
+++ src/main/java/com/BikkadIT/FirstgitjiraprojectApplication.java
@@ -0,0 +1,13 @@
+package com.BikkadIT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstgitjiraprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstgitjiraprojectApplication.class, args);
+	}
+
+}
